cdc865bc993c2bea99c106147571fafa1501d9e6
dksxodnr234 <dksxodnr234@gmail.com>
2021-06-19 21:30:16 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/*  Whole-script strict mode syntax
+    JavaScript is very flexible
+    flexible == dangerous sometimes
+    'use strict' is added ECMAScript 5
+*/  
+
+'use strict';
+console.log("Hello World!");
